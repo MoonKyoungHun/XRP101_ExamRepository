1bf653be7b4087cfbce12039f8f772fc3ad1c6c0
MoonKyoungHun <lsf8355@gmail.com>
2024-11-19 15:11:11 +0900

    [Fix]: 플레이어 수정
    플레이어 프리팹 수정 및 코드 변경

--- Q_03/Assets/Scripts/PlayerController.cs
+++ Q_03/Assets/Scripts/PlayerController.cs
@@ -8,16 +8,18 @@ public class PlayerController : MonoBehaviour
     [field: Range(0, 100)]
     public int Hp { get; private set; }
 
-    private AudioSource _audio;
+    [SerializeField] AudioSource _audio;
+    [SerializeField] GameObject player;
 
     private void Awake()
     {
+        player.GetComponent<GameObject>();
         Init();
     }
 
     private void Init()
     {
-        _audio = GetComponent<AudioSource>();
+        _audio = player.GetComponent<AudioSource>();
     }
     
     public void TakeHit(int damage)
